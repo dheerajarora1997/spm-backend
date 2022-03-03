2606819a4f0ff104673829e17c7fc0092b464d5a
Dheeraj arora <dheeraj.arora1997@yahoo.com>
2022-03-03 11:38:54 +0530

    dashboard page added

--- src/js/theme.js
+++ src/js/theme.js
@@ -6,9 +6,41 @@ if (jQuery('.selectpicker').length) {
     jQuery('.selectpicker').selectpicker();
 }
 
+jQuery('[data-toggle="tooltip"][data-trigger="hover"]').on('click', function () {
+    jQuery('.tooltip').remove();
+})
+
 if (jQuery('.owl-carousel').length) {
-    jQuery('.owl-carousel').owlCarousel();
+    jQuery('.owl-carousel').owlCarousel({
+        nav: true,
+        navigation: true,
+        autoWidth: false,
+        navText: [
+            "<i class='fa fa-angle-left'></i>",
+            "<i class='fa fa-angle-right'></i>"
+        ],
+        autoplay: false,
+        responsiveClass: true,
+        autoplayHoverPause: true,
+        dots: false,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1300: {
+                items: 4
+            }
+        },
+    });
 }
+
+
 
 jQuery(document).ready(function () {
     function openSidebar() {
